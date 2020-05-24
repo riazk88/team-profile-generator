1696eda754e8d1694503ae7780abb2f4ea18f818
Kiran88 <62086331+riazk88@users.noreply.github.com>
2020-05-24 15:05:13 -0600

    added icons

--- dist/generateHTML.js
+++ dist/generateHTML.js
@@ -1,74 +1,102 @@
 function generateHTML () {
     return `<!DOCTYPE html>
     <html lang="en-us">
-    
-    <head>
-    
-      <meta charset="UTF-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-      <title>Team Profile</title>
-      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-    
-    </head>
-    
-    <body>
-        <header>
-          <nav class="navbar navbar-dark bg-primary">
-            <span class="navbar-brand mb-0 h1">Team Profile</span>
-          </nav>
-        </header>
-        <div class="container">
-          <div class="row">
-            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
-              <div class="card text-center">
-                <div class="card-block">
-                  <img src="">
-                  <div class="card-title">
-                    <h4>${data.name}</h4>
+        
+        <head>
+        
+          <meta charset="UTF-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+          <title>Team Profile</title>
+          <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+        
+        </head>
+        
+        <body>
+            <header>
+              <nav class="navbar navbar-dark bg-primary">
+                <span class="navbar-brand mb-0 h1">Team Profile</span>
+              </nav>
+            </header>
+            <div class="container" style= "margin-top:50px">
+              <div class="row">
+                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+                  <div class="card text-center">
+                    <div class="card-block">
+                      <img src="">
+                      <div class="card-title">
+                        <h4>${data.name}</h4>
+                      </div>
+                      <div class="card-text"><svg class="bi bi-briefcase-fill" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" d="M0 12.5A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5V6.85L8.129 8.947a.5.5 0 0 1-.258 0L0 6.85v5.65z"/>
+                        <path fill-rule="evenodd" d="M0 4.5A1.5 1.5 0 0 1 1.5 3h13A1.5 1.5 0 0 1 16 4.5v1.384l-7.614 2.03a1.5 1.5 0 0 1-.772 0L0 5.884V4.5zm5-2A1.5 1.5 0 0 1 6.5 1h3A1.5 1.5 0 0 1 11 2.5V3h-1v-.5a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5V3H5v-.5z"/>
+                      </svg> ${data.role}</div>
+                      <br>
+                      <ul class="list-group list-group-flush">
+                      <li class="list-group-item"><svg class="bi bi-envelope" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" d="M14 3H2a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4a1 1 0 0 0-1-1zM2 2a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2H2z"/>
+                        <path d="M.05 3.555C.017 3.698 0 3.847 0 4v.697l5.803 3.546L0 11.801V12c0 .306.069.596.192.856l6.57-4.027L8 9.586l1.239-.757 6.57 4.027c.122-.26.191-.55.191-.856v-.2l-5.803-3.557L16 4.697V4c0-.153-.017-.302-.05-.445L8 8.414.05 3.555z"/>
+                      </svg> Email:<a href="mailto:lollypop@yahoo.com">${data.email}</a></li>
+                      <li class="list-group-item">ID: ${data.id}</li>
+                      <li class="list-group-item">Office Number: ${data.officeNumber}</li>
+                      </ul>
+                    </div>
                   </div>
-                  <div class="card-text">${data.role}</div>
-                  <br>
-                  <div class="card-text">Email:</div><a href="#">${data.email}</a>
-                  <div class="card-text">ID: ${data.id}</div>
-                  <div class="card-text">Office Number: ${data.officeNumber}</div>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+                  <div class="card text-center">
+                    <div class="card-block">
+                      <img src="">
+                      <div class="card-title">
+                        <h4>${data.name}</h4>
+                      </div>
+                      <div class="card-text"><svg class="bi bi-display-fill" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M5.75 13.5c.167-.333.25-.833.25-1.5h4c0 .667.083 1.167.25 1.5H11a.5.5 0 0 1 0 1H5a.5.5 0 0 1 0-1h.75z"/>
+                        <path fill-rule="evenodd" d="M13.991 3H2c-.325 0-.502.078-.602.145a.758.758 0 0 0-.254.302A1.46 1.46 0 0 0 1 4.01V10c0 .325.078.502.145.602.07.105.17.188.302.254a1.464 1.464 0 0 0 .538.143L2.01 11H14c.325 0 .502-.078.602-.145a.758.758 0 0 0 .254-.302 1.464 1.464 0 0 0 .143-.538L15 9.99V4c0-.325-.078-.502-.145-.602a.757.757 0 0 0-.302-.254A1.46 1.46 0 0 0 13.99 3zM14 2H2C0 2 0 4 0 4v6c0 2 2 2 2 2h12c2 0 2-2 2-2V4c0-2-2-2-2-2z"/>
+                        <path d="M2 4h12v6H2z"/>
+                      </svg> ${data.role}</div>
+                      <br>
+                      <ul class="list-group list-group-flush">
+                      <li class="list-group-item"><svg class="bi bi-envelope" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" d="M14 3H2a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4a1 1 0 0 0-1-1zM2 2a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2H2z"/>
+                        <path d="M.05 3.555C.017 3.698 0 3.847 0 4v.697l5.803 3.546L0 11.801V12c0 .306.069.596.192.856l6.57-4.027L8 9.586l1.239-.757 6.57 4.027c.122-.26.191-.55.191-.856v-.2l-5.803-3.557L16 4.697V4c0-.153-.017-.302-.05-.445L8 8.414.05 3.555z"/>
+                      </svg> Email:<a href="mailto:bob@yahoo.com">${data.email}</a></li>
+                      <li class="list-group-item">ID: ${data.id}</li>
+                      <li class="list-group-item">GitHub: <a href="https://github.com/bobo">${data.github}</a></li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+                  <div class="card text-center">
+                    <div class="card-block">
+                      <img src="">
+                      <div class="card-title">
+                        <h4>${data.name}</h4>
+                      </div>
+                      <div class="card-text"><svg class="bi bi-pencil-square" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456l-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+                        <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
+                      </svg> ${data.role}</div>
+                      <br>
+                      <ul class="list-group list-group-flush">
+                      <li class="list-group-item"><svg class="bi bi-envelope" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" d="M14 3H2a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4a1 1 0 0 0-1-1zM2 2a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2H2z"/>
+                        <path d="M.05 3.555C.017 3.698 0 3.847 0 4v.697l5.803 3.546L0 11.801V12c0 .306.069.596.192.856l6.57-4.027L8 9.586l1.239-.757 6.57 4.027c.122-.26.191-.55.191-.856v-.2l-5.803-3.557L16 4.697V4c0-.153-.017-.302-.05-.445L8 8.414.05 3.555z"/>
+                      </svg> Email:<a href="mailto:maya@yahoo.com">${data.email}</a></li>
+                      <li class="list-group-item">ID: ${data.id}</li>
+                      <li class="list-group-item">School: ${data.school}</li>
+                      </ul>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
-            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
-              <div class="card text-center">
-                <div class="card-block">
-                  <img src="">
-                  <div class="card-title">
-                    <h4>${data.name}</h4>
-                  </div>
-                  <div class="card-text">${data.role}</div>
-                  <br>
-                  <div class="card-text">Email:</div><a href="#">${data.email}</a>
-                  <div class="card-text">ID: ${data.id}</div>
-                  <div class="card-text">GitHub: </div><a href="#">${data.github}</a>
-                </div>
-              </div>
-            </div>
-            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
-              <div class="card text-center">
-                <div class="card-block">
-                  <img src="">
-                  <div class="card-title">
-                    <h4>${data.name}</h4>
-                  </div>
-                  <div class="card-text">${data.role}</div>
-                  <br>
-                  <div class="card-text">Email:</div><a href="#">${data.email}</a>
-                  <div class="card-text">ID: ${data.id}</div>
-                  <div class="card-text">School: ${data.school}</div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-    </body>
-    
+            <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+            <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+            <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+        </body>
+        
     </html>`
 };
 
